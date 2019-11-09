8f6c525278de6ca82ce821c387b25634a4ff3ffa
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-09 03:11:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'pp'
 
 def directors_totals(nds)
 
-  pp directors_database
+  #pp directors_database
   director_final = {}
   director_index = 0
   while director_index < directors_database.length do
