20817c124d09968055d0d5d3ac6fa181dbeca1f3
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-09 03:04:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,23 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
+
+  pp directors_database
+  director_final = {}
+  director_index = 0
+  while director_index < directors_database.length do
+    director = [directors_database[director_index][:name]]
+    director_gross = 0
+    pp director
+    film = 0
+    while film < directors_database[director_index][:movies].length do
+      pp directors_database[director_index][:movies][film]
+    end
+
+    director_index += 1
+  end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
